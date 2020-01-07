cc052947affa2dbdfde32e38eb8a4989dd243f73
zhaohui <zhaohuiq@yonyou.com>
2020-01-07 17:14:16 +0800

    内饰检查

--- pages/vehicle-inspection/components/interior-check.vue
+++ pages/vehicle-inspection/components/interior-check.vue
@@ -1,20 +1,108 @@
 <template>
-	<view>
-		<!-- 内饰检查 -->
-	</view>
+  <view class="page-wrap">
+    <view class="container">
+      <view class="content">
+       <!-- <view class="m-flex m-align-center m-height-80 content-top">客户信息</view> -->
+        <view class="subContent">
+          <VChoose></VChoose>
+       <!-- <view class="uni-flex uni-row">
+          <view class="flex-item1  sub-text">灯光：</view>
+          <view class="flex-item2  sub-text ">
+            <button size="mini" type="primary" plain="true" style="width:32%;margin-right: 2%;">正常</button>
+            <button size="mini" type="primary" plain="true" style="width:32%;margin-right: 2%;">观察</button>
+            <button size="mini" type="warn" plain="true" style="width:32%;">异常</button>
+          </view>
+          <view class="flex-item3  sub-text ">何红</view>
+        </view> -->
+        
+       
+        </view>
+      </view>
+     <view class="content">
+       <view class="m-flex m-align-center m-height-80 content-top">内饰检查</view>
+       <view class="subContent">
+         <textarea class="sub-textarea label-pad" @blur="bindTextAreaBlur" auto-height />
+         <button type="warn " class="btn-blue label-pad ">按下说话</button>
+       </view>
+     </view>
+     
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-    name:'InteriorCheck',
-		data() {
-			return {
-				
-			};
-		}
-	}
+  import VChoose from './v-choose.vue'
+export default {
+	name:'InteriorCheck',
+  components:{
+    VChoose
+  },
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style lang="scss">
-
+.page-wrap {
+  min-height: 100vh;
+  background-color: $uni-bg-color-page;
+  margin-bottom: 10vh;
+  .container {
+    padding: 20rpx;
+    .content {
+      background-color: #ffffff;
+      margin: 20rpx;
+      border-radius: 20rpx;
+      padding: 0 20rpx;
+      .content-top {
+        padding-right: 10rpx;
+        height: 100rpx;
+        line-height: 100rpx;
+        font-size: 42rpx;
+        font-weight: bold;
+      }
+      .subContent {
+        margin: 0 auto 20rpx;
+        padding: 10rpx 0;
+        .flex-item1 {
+          width: 20%;
+          padding: 5rpx 0;
+          text-align: center;
+        }
+        .flex-item2{
+          width: 66%;
+          padding: 5rpx 0;
+        }
+        .flex-item3{
+          width: 14%;
+          text-align: center;
+          padding: 5rpx 0;
+        }
+        .label-pad {
+          margin-bottom: 20rpx;
+        }
+        .sub-text {
+          line-height: 60rpx;
+        }
+        .sub-textarea {
+          display: block;
+          background-color: #f1f1f1;
+          min-height: 15vh;
+          width: 100%;
+          border-radius: 20rpx;
+          padding: 20rpx;
+          line-height: 40rpx;
+        }
+        .btn-blue {
+          width: 80%;
+          text-align: center;
+          background-color: #0099cc;
+          color: $uni-text-color-inverse;
+        }
+        
+      }
+    }
+  }
+}
 </style>
---
+++ pages/vehicle-inspection/components/v-choose.vue
@@ -0,0 +1,84 @@
+<template>
+  <view>
+    <view class="uni-flex uni-row">
+      <view class="flex-item1 label-pad">灯光：</view>
+      <view class="flex-item2 label-pad">*</view>
+      <view class="flex-item3 label-pad"><button size="mini" plain="true" :class="data1==='normal'?'btn-primary':'btn-gary'" @click="rowClick('normal')">正常</button></view>
+      <view class="flex-item3 label-pad"><button size="mini" plain="true" :class="data1==='warn'?'btn-warn':'btn-gary'" @click="rowClick('warn')">观察</button></view>
+      <view class="flex-item3 label-pad"><button size="mini" plain="true" :class="data1==='error'?'btn-error':'btn-gary'"  @click="rowClick('error')">异常</button></view>
+      <view class="flex-item4"><uni-icons type="camera" size="30" color="#CCCCCC"></uni-icons></view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data1:'normal'
+    };
+  },
+  methods:{
+    rowClick(el){
+      this.data1 = el;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.flex-item1 {
+  width: 20%;
+  text-align: center;
+  line-height: 60rpx;
+}
+.flex-item2 {
+  width: 3%;
+  color: #ff0000;
+  line-height: 60rpx;
+}
+.flex-item3 {
+  width: 21%;
+}
+.flex-item4 {
+  width: 14%;
+  text-align: center;
+}
+.label-pad {
+  margin-right: 2%;
+}
+.btn-gary {
+  display: block;
+  line-height: 2.3;
+  font-size: 13px;
+  padding: 0;
+  color: #CCCCCC;
+  border: 2rpx solid #CCCCCC;
+}
+
+.btn-primary {
+  display: block;
+  line-height: 2.3;
+  font-size: 13px;
+  padding: 0;
+  color: #0099cc;
+  border: 2rpx solid #0099cc;
+}
+.btn-warn {
+  display: block;
+  line-height: 2.3;
+  font-size: 13px;
+  padding: 0;
+  color: #ffcc00;
+  border: 2rpx solid #ffcc00;
+}
+.btn-error {
+  display: block;
+  line-height: 2.3;
+  font-size: 13px;
+  padding: 0;
+  color: #ff6666;
+  border: 2rpx solid #ff6666;
+}
+
+</style>
