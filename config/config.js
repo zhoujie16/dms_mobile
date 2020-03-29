9092ace5629e23e15dcae06af9504c8ab5845a22
zhoujie <zhoujie16m@163.com>
2020-03-29 13:28:40 +0800

    修改: mock 地址

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@
  * 在Vue组件中 可通过 this.$appConfig.xxx 访问
  */
 const devConfig = {
-  // requsetUrl: "https://mock.yonyoucloud.com/mock/3597", // 请求地址
-  requsetUrl: "https://cscyx.yycsy.com",  
+  requsetUrl: "https://mock.yonyoucloud.com/mock/3597", // 请求地址
+  // requsetUrl: "https://cscyx.yycsy.com",  
 };
 
 //生产环境
