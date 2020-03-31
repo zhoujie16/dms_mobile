0df4e77a8adf4a664f1a2955cd7424c3149abce0
zhoujie <zhoujie16m@163.com>
2020-03-31 14:50:30 +0800

    Create cell.vue

---
+++ pages/demo/ui-demo/cell.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- Cell 单元格 -->
+  <MPage ref="MPage">
+     
+    
+  </MPage>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss"></style>
