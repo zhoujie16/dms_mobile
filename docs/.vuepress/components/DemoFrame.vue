7cc5b884d5a22849e2b8663cf639dbbc965c78cb
zhoujie <zhoujie16m@163.com>
2020-04-28 18:20:08 +0800

    新增: 文档

---
+++ docs/.vuepress/components/DemoFrame.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="demo-frame">
+    <iframe
+      class="demo-frame_inner"
+      :src="srcUrl"
+      width="375"
+      height="667"
+      scrolling="no"
+      frameborder="0"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DemoFrame",
+  props: {
+    src: {
+      type: String,
+      default: "/ui-demo"
+    }
+  },
+  computed: {
+    srcUrl() {
+      return this.baseUrl + this.src;
+    }
+  },
+  mounted() {},
+  data() {
+    return {
+      // baseUrl: "http://dms-mob-demo.zhoujie16.cn/#/pages/demo/ui-demo",
+      baseUrl: "http://127.0.0.1:8081/#/pages/demo/ui-demo"
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+.demo-frame {
+  position: fixed;
+  right: 2rem;
+  top: 5rem;
+  border: solid 1px #dddddd;
+  border-radius: 6px;
+  overflow: hidden;
+  transform: scale(0.8) translate(0px, -50px);
+  box-shadow: #ebedf0 0 4px 12px;
+}
+
+@media (max-width: 750px) {
+  .demo-frame {
+    display: none;
+  }
+}
+</style>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  title: "DMS Mobile 开发文档",
+  description: "网站的描述",
+  head: [["script", { src: "/js/jquery.min.js" }]],
+  themeConfig: {
+    navbar: true,
+    displayAllHeaders: false,
+    sidebar: [
+      {
+        title: "开发指南",
+        collapsable: false,
+        children: [["/guide/", "开发指南"]]
+      },
+      {
+        title: "基础组件",
+        collapsable: false,
+        children: [
+          ["/markdown/m-page.md", "页面"],
+          ["/markdown/m-button.md", "按钮"],
+          ["/markdown/m-label.md", "单元格"],
+          ["/markdown/m-input.md", "文本框"],
+          ["/markdown/m-textarea.md", "文本域"],
+          ["/markdown/m-checkbox.md", "单行选择"],
+          ["/markdown/m-checkbox-panel.md", "多行选择"],
+          ["/markdown/m-picker.md", "时间日期选择"],
+          ["/markdown/validator.md", "表单校验"],
+          ["/markdown/m-swipe-cell.md", "滑动单元格"],
+          ["/markdown/m-dialog.md", "弹窗交互"],
+          ["/markdown/swiper-tab.md", "选项卡"],
+          ["/markdown/navbar.md", "导航栏"]
+        ]
+      },
+      {
+        title: "业务组件",
+        collapsable: false,
+        children: [
+          ["/markdown/m-license.md", "车牌号"],
+          ["/markdown/search-filter.md", "查询"],
+          ["/markdown/m-vehicle-check-cell.md", "车辆检查项"],
+          ["/markdown/dict.md", "数据字典"],
+          ["/markdown/doc-reader.md", "文档阅读器"]
+        ]
+      }
+    ]
+  },
+  lastUpdated: "Last Updated",
+  host: "127.0.0.1",
+  port: 8001
+};
